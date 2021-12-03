bedbde0552f2af748db58a0b4e69c90c8ca719bb
Muxa172002 <muxa172002@yandex.ru>
2021-12-03 20:08:29 +0300

    1

---
+++ ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyelena <cyelena@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 17:33:42 by cyelena           #+#    #+#             */
+/*   Updated: 2021/12/03 19:52:16 by cyelena          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int	ft_printf(const char *arg, ...);
+#endif
